e13f409805e1dee5f9d3d18879c96b9adb6b0d58
simardo <simardo@gmail.com>
2017-11-27 21:38:04 -0500

    added weight and distribution

--- src/app/components/welcome/welcome.ts
+++ src/app/components/welcome/welcome.ts
@@ -11,17 +11,27 @@ interface Operation {
     first: number;
     second: number;
     operator: Operator;
-    result: number;
+    answer?: number;
+    correction?: number;
 }
 
 @WithRender
 @Component
 export default class Welcome extends Vue {
-    private max: number = 11;
+    private weight: number[] = [1, 1, 1, 2, 2, 3, 5, 7, 8, 9, 10]; // 1-11
+    private distribution: number[] = [];
     private currentOperation: Operation | null = null;
     private answer: number = 0;
     private success: Operation[] = [];
     private fails: Operation[] = [];
+
+    protected created(): void {
+        this.weight.forEach((v, i) => {
+            for (let w: number = 0; w < v; w++) {
+                this.distribution.push(i + 1);
+            }
+        });
+    }
 
     protected beforeMount(): void {
         this.doNext();
@@ -36,39 +46,36 @@ export default class Welcome extends Vue {
             let operator: Operator = Math.round(Math.random()) == 0 ? Operator.Add : Operator.Sub;
             let first: number;
             let second: number;
+            let max = this.distribution[Math.floor(Math.random() * this.distribution.length)];
             if (operator == Operator.Add) {
-                let res = Math.round(Math.random() * this.max);
+                let res = max;
                 first = Math.round(Math.random() * res);
                 second = res - first;
             } else {
-                first = Math.round(Math.random() * this.max);
+                first = max;
                 second = Math.round(Math.random() * first);
             }
             this.currentOperation = {
                 first: first,
                 second: second,
-                operator: operator,
-                result: -1
+                operator: operator
             };
-            console.log('t', this.currentOperation);
         }
     }
 
     private onSubmit(): void {
-        let res: number;
         let op: Operation = this.currentOperation as Operation;
 
         if (op.operator == Operator.Add) {
-            res = op.first + op.second;
+            op.correction = op.first + op.second;
         } else {
-            res = op.first - op.second;
+            op.correction = op.first - op.second;
         }
-        op.result = this.answer;
+        op.answer = this.answer;
 
         this.doNext();
 
-        console.log(op);
-        if (res == op.result) {
+        if (op.correction == op.answer) {
             this.success.push(op);
         } else {
             this.fails.push(op);
@@ -84,6 +91,6 @@ export default class Welcome extends Vue {
     }
 
     private getOperationToString(operation: Operation): string {
-        return `${operation.first} ${operation.operator} ${operation.second} = ${operation.result}`;
+        return `${operation.first} ${operation.operator} ${operation.second} = ${operation.answer}`;
     }
 }
--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,7 @@ import Vue from 'vue';
 import router from './router';
 import Button from '@ulaval/modul-components/dist/components/button/button';
 import TextField from '@ulaval/modul-components/dist/components/textfield/textfield';
-import i18n, {currentLang, FRENCH}  from '@ulaval/modul-components/dist/utils/i18n/i18n';
+import i18n, { currentLang, FRENCH } from '@ulaval/modul-components/dist/utils/i18n/i18n';
 import fr from '@ulaval/modul-components/dist/lang/fr';
 
 Vue.config.productionTip = false;
